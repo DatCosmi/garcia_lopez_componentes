47700c1e9a8da958f0205542eee025d5c133bb2a
Ivan Garcia <ivan03idk@gmail.com>
2023-01-19 11:52:20 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Carlos Iván García López</h1>
+      <h2>Práctica Componentes</h2>
+      <div>
+        <Button as="a" href="https://utd.edu.mx" primary>
+          UTD
+        </Button>
+        <Button as="a" href="https://github.com/DatCosmi" primary>
+          GitHub
+        </Button>
+        <Button as="a" href="https://www.instagram.com/datcosmi" primary>
+          Instagram
+        </Button>
+      </div>
     </div>
   );
 }
